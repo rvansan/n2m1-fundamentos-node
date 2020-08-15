0d39e6b28be8ecb3152a9da932d6d77dd97c1130
Rafael Vansan <vansan.rafael@gmail.com>
2020-08-15 14:05:00 -0300

    commit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw new Error('You do not have enough balance');
+    }
+    if (!title) {
+      throw Error('Field title is required!');
+    }
+    if (!value) {
+      throw Error('Field value is required!');
+    }
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('This type is not valid!');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
